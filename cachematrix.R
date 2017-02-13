0e5997f18ba7b8bc425e3beab0b9a31ad0129843
Bong Walsh <Bong Walsh>
2017-02-12 19:51:47 -0800

    adding cachematrix fo the 5000th time

---
+++ cachematrix.R
@@ -0,0 +1,39 @@
+## This function creates a special matrix capable of caching its inverse.
+
+## set - sets value of the matrix
+## get - gets value of the matrix
+## setinv - sets the inverse of the matrix
+## getinv - gets the value of the matrix
+
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) inv <<- solve
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
+}
+
+## This function computes the inverse of the matrix returned by makeCacheMatrix.
+## If the inverse has already been calculated, and the matrix has not been changed,
+## it will simply retrieve the inverse from the cache.
+
+cacheSolve <- function(x, ...) {
+  ## attempt to retrieve inverse from cache
+  inv <- x$getinv()  
+  ## if inverse is in cache, it is returned  
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)   
+  }
+  ## if inverse is not in cache, it is calculated
+  data <- x$get()
+  inv <- solve(data, ...)  ## calculation of inverse
+  x$setinv(inv)
+  inv   
+}
